f5179bdddf9da99fbd87608a7ee48f5893ffc455
Unsar Malik <unsar.malik24@gmail.com>
2019-05-05 20:41:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,7 +38,7 @@ def self.find_or_create_by_name(name)
   self.find_by_name(name) || self.create_by_name(name)
 end
 
-def self.alphabetical(name)
+def self.alphabetical
   self.sort_by {|song| song.name}
 end
 end
