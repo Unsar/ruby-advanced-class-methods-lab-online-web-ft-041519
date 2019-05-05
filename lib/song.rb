523fdf8e32d99405b8b4a1266080f3a2930ea2fb
Unsar Malik <unsar.malik24@gmail.com>
2019-05-05 20:54:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,6 +50,11 @@ def self.new_from_filename(name)
 end
 
 def self.create_from_filename
+  song=self.new
+  song.name=name.split("- ")[1].chomp(".mp3")
+  song.artist_name = name.split(" - ")[0]
+  @@all << song
+  song
 end
 
 def self.destroy_all
